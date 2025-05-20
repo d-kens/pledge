5f6fdf735dcc25fde2505f90203e008a4e0f02d7
d-kens <omondikens1@gmail.com>
2025-05-20 16:05:54 +0300

    configure cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const allowedOrigins = (process.env.ALLOWED_ORIGINS || '')
+    .split(',')
+    .map(origin => origin.trim())
+    .filter(origin => origin); 
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
+    allowedHeaders: ["content-type", "authorization"],
+    credentials: true,
+  });
+
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
-bootstrap();
+bootstrap().then(() =>
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${process.env.PORT}`,
+  ),
+);
